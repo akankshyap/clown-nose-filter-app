896fa348d2f169eb75f3b59f0793f9e7e2d3eb54
akankshyap <87641487+akankshyap@users.noreply.github.com>
2021-10-26 13:44:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotPoses)
+
+
+}
+function modelloaded(){
+    console.log ("poseNet")
+}
+function gotPoses (results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x ="+ results[0].pose.nose.x);
+        console.log("nose y ="+ results[0].pose.nose.y);
+    }
+}
+
+function draw(){
+    image(video,00,00,300,300);
+
+}
+
+function takesnapshot(){
+    save('pic.png');
+}
